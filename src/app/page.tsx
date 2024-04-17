70fe297663cd29d01a8fa4dc0a7edcb1da5581f6
Yash Sonalia <sonaliayash@gmail.com>
2024-04-17 10:55:12 +0530

    (add) force dynamic page render + vscode ts version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
-  "https://utfs.io/f/92e928b8-dd39-4ac4-af4f-913aef230485-3flas7.jpg",
-  "https://utfs.io/f/7af7af71-e65e-43fc-aeb3-c36e4d964ede-w2psx9.jpg",
-  "https://utfs.io/f/43b855e4-d059-4867-b629-338ee3bb21cc-167z63.jpg",
+  //   "https://utfs.io/f/92e928b8-dd39-4ac4-af4f-913aef230485-3flas7.jpg",
+  //   "https://utfs.io/f/7af7af71-e65e-43fc-aeb3-c36e4d964ede-w2psx9.jpg",
+  //   "https://utfs.io/f/43b855e4-d059-4867-b629-338ee3bb21cc-167z63.jpg",
   "https://utfs.io/f/4a81abef-e8c5-4380-aa32-6d8cac48ff09-qsaiy1.jpg",
 ];
+
 
 const mockImages = mockUrls.map((url, index) => {
   return {
